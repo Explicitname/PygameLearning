e20fb3edc51e9f32692872d4c83a724646396fa0
Explicitname <explicit6911@gmail.com>
2015-08-20 10:39:29 -0400

    Add changing colors of rectangle on spacebar keypress

--- rectangles.py
+++ rectangles.py
@@ -3,12 +3,19 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 quitGame = False
+isBlue = False
 
 while not quitGame:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quitGame = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            isBlue = not isBlue
             
-    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+    if isBlue: 
+        color = (0, 128, 255)
+    else: 
+        color = (255, 100, 0)
+    pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
     
     pygame.display.flip()
